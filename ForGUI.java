49fb8f336016be2d5c0d7f5bc5a6dfb3cccf1b5c
FatinAqilah14 <nrftinq@student.usm.my>
2023-01-19 18:57:34 +0800

    try commit

--- ForGUI.java
+++ ForGUI.java
@@ -1,9 +1,57 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ForGUI {
-    private JTextField tfFirstName;
-    private JTextField tfLastName;
+import static javax.swing.JOptionPane.*;
+
+public class ForGUI extends JFrame {
+    private JTextField username;
+    private JTextField password;
     private JButton btnOK;
+//    {
+//
+//        if (username.getText().equals("") && password.getText().equals(""))
+//        {
+//            JOptionPane.showMessageDialog(this, "Blank is not alowed");
+//        }
+//    };
+
     private JButton btnClear;
     private JLabel lbWelcome;
+    private JPanel mainPanel;
+
+    public ForGUI() {
+        setContentPane(mainPanel);
+        setTitle("WELCOME");
+        setSize(450,300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (username.getText().equals("") && password.getText().equals(""))
+            {
+                showMessageDialog("Blank is not allowed");
+            }
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                username.setText("");
+                password.setText("");
+            }
+        });
+    }
+
+    private void showMessageDialog(String blank_is_not_allowed) {
+    }
+
+
+    public static void main(String[] args) {
+        ForGUI myGUI = new ForGUI();
+    }
+
 }
